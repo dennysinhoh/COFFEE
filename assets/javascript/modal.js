3166086fb3c230cd6512333b6644ae95df5a64b3
dennysinhohe <dennys_almeida_souza@hotmail.com>
2023-05-26 01:05:18 -0300

    front finish

---
+++ assets/javascript/modal.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("meuModal");
+
+var btn = document.getElementById("Atendimento");
+
+var span = document.getElementsByClassName("fechar")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
